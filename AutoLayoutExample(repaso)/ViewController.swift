0a660af8d5b4718218545657fe5012053a075f1d
Eduardo Jordan <eduardojordan@hotmail.com>
2018-10-06 22:53:49 +0200

    Detallando StacKView y buenas practicas Apple

---
+++ AutoLayoutExample(repaso)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutExample(repaso)
+//
+//  Created by Eduardo on 6/10/18.
+//  Copyright © 2018 Eduardo Jordan Muñoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
